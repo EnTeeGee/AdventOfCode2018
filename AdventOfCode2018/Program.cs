959390ba25c979696f7c2c98a1aff2cb5ed46353
EnTeeGee <nickdyke2@gmail.com>
2018-12-01 13:34:09 +0000

    Initial commit of AdventOfCode2018 repo.
    Contains Visual Studio structure and some classes to handle selection of the correct solution to run.

---
+++ AdventOfCode2018/Program.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode2018.Common;
+using System;
+
+namespace AdventOfCode2018
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var manager = new Manager();
+            manager.Init();
+        }
+    }
+}
