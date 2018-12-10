657a4225ae1b55f382e2694bdbb9ea3dfd6f0933
EnTeeGee <nickdyke2@gmail.com>
2018-12-10 20:59:41 +0000

    Add solutions for day 9 and 10

---
+++ AdventOfCode2018/Solutions/Day09.cs
@@ -0,0 +1,87 @@
+﻿using AdventOfCode2018.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2018.Solutions
+{
+    
+    class Day09
+    {
+        [Solution(9, 1)]
+        public long Problem1(string input)
+        {
+            var info = input.Split(' ');
+            var players = int.Parse(info[0]);
+            var steps = int.Parse(info[6]);
+
+            var currentNode = new RingNode(0);
+            currentNode.Next = currentNode;
+            currentNode.Prev = currentNode;
+
+            var scores = Enumerable.Range(0, players).ToDictionary(it => it, it => (long)0);
+
+            for(var i = 1; i <= steps; i++)
+            {
+                if(i % 23 == 0)
+                {
+                    var toRemove = currentNode.Prev.Prev.Prev.Prev.Prev.Prev.Prev;
+                    currentNode = toRemove.Next;
+                    toRemove.Remove();
+                    var currentPlayer = i % players;
+                    scores[currentPlayer] = scores[currentPlayer] + i + toRemove.Value;
+                }
+                else
+                {
+                    var addedNode = new RingNode(i);
+                    addedNode.InsertAfter(currentNode.Next);
+                    currentNode = addedNode;
+                }
+            }
+
+            return scores.Values.Max();
+        }
+
+        [Solution(9, 2)]
+        public long Problem2(string input)
+        {
+            var info = input.Split(' ');
+            var players = int.Parse(info[0]);
+            var steps = int.Parse(info[6]);
+
+            return Problem1($"{players} Players; last marble is worth {steps * 100} points");
+        }
+
+        class RingNode
+        {
+            public int Value { get; }
+
+            public RingNode Next { get; set; }
+
+            public RingNode Prev { get; set; }
+
+            public RingNode(int value)
+            {
+                this.Value = value;
+            }
+
+            public void InsertAfter(RingNode item)
+            {
+                Next = item.Next;
+                Next.Prev = this;
+                Prev = item;
+                Prev.Next = this;
+            }
+
+            public void Remove()
+            {
+                Prev.Next = Next;
+                Next.Prev = Prev;
+                Prev = null;
+                Next = null;
+            }
+        }
+    }
+}
---
+++ AdventOfCode2018/Solutions/Day10.cs
@@ -0,0 +1,122 @@
+﻿using AdventOfCode2018.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2018.Solutions
+{
+    class Day10
+    {
+        [Solution(10, 1)]
+        public string Problem1(string input)
+        {
+            var lights = input.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Select(it => new Light(it)).ToArray();
+
+            var highest = lights.Max(it => it.X);
+            var seconds = 0;
+
+            while (true)
+            {
+                foreach (var item in lights)
+                    item.StepForward();
+                
+
+                var newHighest = lights.Max(it => it.X);
+                if (newHighest > highest)
+                {
+                    foreach (var item in lights)
+                        item.StepBack();
+                    return DumpLights(lights, seconds);
+                }
+
+                highest = newHighest;
+                ++seconds;
+            }
+        }
+
+        [Solution(10, 2)]
+        public int Problem2(string input)
+        {
+            var lights = input.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries).Select(it => new Light(it)).ToArray();
+
+            var highest = lights.Max(it => it.X);
+            var seconds = 0;
+
+            while (true)
+            {
+                foreach (var item in lights)
+                    item.StepForward();
+
+
+                var newHighest = lights.Max(it => it.X);
+                if (newHighest > highest)
+                {
+                    foreach (var item in lights)
+                        item.StepBack();
+                    return seconds;
+                }
+
+                highest = newHighest;
+                ++seconds;
+            }
+        }
+
+        private string DumpLights(Light[] lights, int seconds)
+        {
+            var minX = lights.Min(it => it.X);
+            var minY = lights.Min(it => it.Y);
+            var maxX = lights.Max(it => it.X);
+            var maxY = lights.Max(it => it.Y);
+            var output = new StringBuilder();
+
+            for(var i = minY; i <= maxY; i++)
+            {
+                for(var j = minX; j <= maxX; j++)
+                {
+                    if (lights.Any(it => it.Y == i && it.X == j))
+                        output.Append('#');
+                    else
+                        output.Append('.');
+                }
+
+                output.Append('\n');
+            }
+
+            output.Append($"Total seconds: {seconds}");
+
+            return output.ToString();
+        }
+
+        private class Light
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public int MoveX { get; set; }
+            public int MoveY { get; set; }
+
+            public Light(string input)
+            {
+                var sections = input.Split(new[] { '<', '>', ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+                X = int.Parse(sections[1]);
+                Y = int.Parse(sections[2]);
+                MoveX = int.Parse(sections[4]);
+                MoveY = int.Parse(sections[5]);
+            }
+
+            public void StepForward()
+            {
+                X += MoveX;
+                Y += MoveY;
+            }
+
+            public void StepBack()
+            {
+                X -= MoveX;
+                Y -= MoveY;
+            }
+        }
+    }
+}
